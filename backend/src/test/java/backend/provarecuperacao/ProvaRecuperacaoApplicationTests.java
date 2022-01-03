ba550eb9e16a390cd1fd97fb38b43e9afbca1ee1
SundaeRaul <raulrios0907@outlook.com>
2022-01-03 11:18:45 -0300

    first commit

---
+++ backend/src/test/java/backend/provarecuperacao/ProvaRecuperacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.provarecuperacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaRecuperacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
